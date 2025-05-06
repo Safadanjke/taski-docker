dd1a218c0b8b37ca5442010b6a0609aa9d3ecf6d
Халуев Некит <khaluev2004@mail.ru>
2025-05-07 02:03:46 +0300

    5 secret_key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,10 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR.parent / '.env')
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
